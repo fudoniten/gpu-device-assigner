717887adcefc994dddb46bebd15b40ff5fd7198d
niten <niten@fudo.org>
2025-07-10 21:26:06 -0700

    Various changes

--- src/gpu-device-assigner/core.clj
+++ src/gpu-device-assigner/core.clj
@@ -2,6 +2,9 @@
   (:require [clojure.string :as str]
             [clojure.stacktrace :refer [print-stack-trace]]
             [clojure.set :refer [subset?]]
+
+            [gpu-device-assigner.logging :as log]
+
             [reitit.ring :as ring]
             [cheshire.core :as json]
             [ring.util.response :as response]
@@ -10,22 +13,21 @@
            java.time.Instant))
 
 (defn get-node
-  [k8s-client node-name]
+  [{:keys [k8s-client]} node-name]
   (k8s/invoke k8s-client {:kind    :Node
                           :action  :get
                           :request {:name node-name}}))
 
-(defn get-node-labels
-  [k8s-client node-name]
-  (-> (get-node k8s-client node-name)
-      :metadata
-      :labels))
-
 (defn get-node-annotations
-  [k8s-client node-name]
-  (-> (get-node k8s-client node-name)
+  [ctx node-name]
+  (-> (get-node ctx node-name)
       :metadata
       :annotations))
+
+(defn base64-encode
+  [str]
+  (let [encoder (Base64/getEncoder)]
+    (.encodeToString encoder (.getBytes str "UTF-8"))))
 
 (defn base64-decode
   [b64-str]
@@ -39,48 +41,55 @@
 (defn parse-json [str] (json/parse-string str true))
 
 (defn get-device-labels
-  [k8s-client node-name]
-  (some->> (get-node-annotations k8s-client node-name)
+  [ctx node-name]
+  (some->> (get-node-annotations ctx node-name)
            :fudo.org/gpu.device.labels
            (base64-decode)
            (parse-json)
            (map-vals set)))
 
 (defn get-device-reservations
-  [k8s-client node-name]
-  (let [annotations (get-node-annotations k8s-client node-name)])
-  (some->> annotations
-           :fudo.org/gpu.device.reservations
-           (parse-json)
-           (assoc (select-keys annotations [:resourceVersion]) :reservations)))
+  [ctx node-name]
+  (let [annotations (get-node-annotations ctx node-name)]
+    (some->> annotations
+             :fudo.org/gpu.device.reservations
+             (parse-json)
+             (assoc (select-keys annotations [:resourceVersion]) :reservations))))
+
+(defn node-patch
+  [{:keys [k8s-client]} node-name patch]
+  (k8s/invoke k8s-client
+              {:kind  :Node
+               :name  node-name
+               :patch patch}))
 
 (defn reserve-device
-  [k8s-client {:keys [node-name namespace pod device-id]}]
-  (let [{version :resourceVersion reservations :reservations} (get-device-reservations k8s-client node-name)]
+  [{:keys [logger] :as ctx} {:keys [node-name namespace pod device-id]}]
+  (let [{version :resourceVersion reservations :reservations} (get-device-reservations ctx node-name)]
     (if-let [reservation (get reservations device-id)]
-      (if (!= (:pod reservation) pod)
-        (do (log/warn (format "device %s is already reserved by pod %s: rejecting" device-id (:pod reservation)))
+      (if (not= (:pod reservation) pod)
+        (do (log/warn logger (format "device %s is already reserved by pod %s: rejecting" device-id (:pod reservation)))
             nil)
-        (do (log/info (format "device %s is already reserved by requesting pod %s: approving!" device-id (:pod reservation)))
+        (do (log/info logger (format "device %s is already reserved by requesting pod %s: approving!" device-id (:pod reservation)))
             true))
       (let [updated-reservations (assoc reservations device-id {:pod pod :namespace namespace :timestamp (Instant/now)})
             reservation-patch    {:metadata {:annotations
                                              {:fudo.org/gpu.device.reservations
                                               (json/generate-string updated-reservations)}}
                                   :resourceVersion version}]
-        (try (do (k8s/patch k8s-client [:Node node-name] patch)
-                 device-id)
+        (try (node-patch ctx node-name reservation-patch)
+             device-id
              (catch Exception e
                (let [status (ex-data e)]
                  (if (= 409 (:status status))
-                   (log/error (format "conflict: reservation was modified before patch was applied. unable to reserve device %s for pod %s."
-                                      device-id pod))
-                   (log/error (format "error %s: unable to reserve device %s for pod %s: %s"
-                                      (:status status) device-id pod (:message e))))
+                   (log/error logger (format "conflict: reservation was modified before patch was applied. unable to reserve device %s for pod %s."
+                                             device-id pod))
+                   (log/error logger (format "error %s: unable to reserve device %s for pod %s: %s"
+                                             (:status status) device-id pod (:message e))))
                  nil)))))))
 
 (defn list-ns-pods
-  [k8s-client namespace]
+  [{:keys [k8s-client]} namespace]
   (k8s/invoke k8s-client {:kind    :Pod
                           :action  :list
                           :request {:namespace namespace}}))
@@ -89,35 +98,38 @@
 ;; (defn watch-pods [k8s-client namespace callback]
 ;;   (k8s-api/watch-namespaced-pod k8s-client namespace {:watch true :callback callback}))
 
-
-(defn get-config-map
-  [k8s-client namespace name]
-  (k8s/invoke k8s-client {:kind    :ConfigMap
-                          :action  :get
-                          :request {:name      name
-                                    :namespace namespace}}))
-
-;; (defn watch-config [k8s-client namespace config-map callback]
-;;   (k8s-api/watch-namespaced-config-map k8s-client
-;;                                        namespace
-;;                                        config-map
-;;                                        {:watch true :callback callback}))
-
 ;; (defn patch-annotation [annotation-path value]
 ;;   [{:op    "add"
 ;;     :path  annotation-path
 ;;     :value value}])
 
-(defn assign-gpu [{:keys [k8s-client node-name pod requested-labels]}]
-  (let [node-dev-labels (get-device-annotation k8s-client node-name)
-        reservations (get-device-reservations k8s-client node-name)
+(defn device-reserved?
+  "A device is reserved if it's assigned to a different pod. Otherwise, it's free."
+  [reservations dev-id this-pod]
+  (some-> reservations
+          (get dev-id)
+          :pod
+          (not= this-pod)))
+
+(defn assign-device [ctx {:keys [node-name pod namespace requested-labels]}]
+  (let [node-dev-labels (get-device-labels ctx node-name)
         candidates (filter (fn [[_ dev-labels]] (subset? requested-labels dev-labels))
                            node-dev-labels)]
     (when (empty? candidates)
       (log/error (format "error: no candidate devices found! pod %s was misassigned. requested labels: [%s]"
                          pod (str/join ", " requested-labels))
                  nil))
-    ))
+    (let [reservations (get-device-reservations ctx node-name)
+          available (filter (fn [dev-id] (not (device-reserved? reservations dev-id pod)))
+                            (keys candidates))
+          selected-id (rand-nth available)]
+      (reserve-device ctx
+                      {:node-name node-name
+                       :namespace namespace
+                       :pod       pod
+                       :device-id selected-id}))))
+
+
 
 ;; (defn try-json-parse [str]
 ;;   (try
@@ -164,17 +176,56 @@
 ;;                           :allowed true}}
 ;;               (try-json-generate)))))))
 
-;; (defn handle-mutation
-;;   [verbose k8s-client]
-;;   (fn [{:keys [kind] :as req}]
-;;     (when-not (= kind "AdmissionReview")
-;;       {:apiVersion "admission.k8s.io/v1"
-;;        :kind       "AdmissionReview"
-;;        :response   {:uid     (get-in req [:request :uid])
-;;                     :allowed false
-;;                     :status {:code    400
-;;                              :message (format "Unexpected request kind: %s" kind)}}})
-;;     ))
+(defn admission-review-response
+  [& {:keys [uid allowed?
+             status message
+             patch]}]
+  (assert (or status patch) "one of :status or :patch must be specified")
+  {:apiVersion "admission.k8s.io/v1"
+   :kind       "AdmissionReview"
+   :response   (if status
+                 {:uid     uid
+                  :allowed allowed?
+                  :status  {:code    status
+                            :message message}}
+                 {:uid       uid
+                  :allowed   allowed?
+                  :patchType "JSONPatch"
+                  :patch     patch})})
+
+(defn device-assignment-patch
+  [device-id]
+  (-> {:op    "add"
+       :path  "/metadata/annotations/cdi.k8s.io~1nvidia.com~1gpu"
+       :value device-id}
+      (json/generate-string)
+      (base64-encode)))
+
+(defn handle-mutation
+  [ctx]
+  (fn [{:keys [kind] :as req}]
+    (when-not (= kind "AdmissionReview")
+      {:apiVersion "admission.k8s.io/v1"
+       :kind       "AdmissionReview"
+       :response   {:uid     (get-in req [:request :uid])
+                    :allowed false
+                    :status {:code    400
+                             :message (format "Unexpected request kind: %s" kind)}}})
+    (let [uid (get-in req [:request :uid])
+          pod (get-in req [:request :object :metadata :name])
+          ns  (get-in req [:request :object :metadata :namespace])
+          annotations (get-in req [:request :object :metadata :annotations])
+          labels (keys (filter (fn [[_ v]] (= v "true")) annotations))
+          node-name (get-in req [:request :object :spec :nodeName])]
+      (if-let [assigned-device (assign-device ctx {:node-name node-name
+                                                   :pod       pod
+                                                   :namespace namespace
+                                                   :requested-labels labels})]
+        (admission-review-response :uid uid :allowed? true
+                                   :patch (device-assignment-patch assigned-device))
+        (admission-review-response :uid uid :status 500 :allowed? false
+                                   :message (format "failed to find unreserved device for pod %s on node %s."
+                                                    pod node-name))))))
 
 ;; (defn app [verbose k8s-client]
 ;;   (ring/ring-handler
@@ -211,3 +262,35 @@
 
 ;; (defn -main [& _]
 ;;   (jetty/run-jetty webhook-handler {:port 8443 :join? false}))
+
+
+;; {
+;;   "apiVersion": "admission.k8s.io/v1",
+;;   "kind": "AdmissionReview",
+;;   "request": {
+;;     "uid": "123abc...",
+;;     "object": {
+;;       "metadata": {
+;;         "name": "mypod",
+;;         "namespace": "default",
+;;         "annotations": {
+;;           "gpu.openai.com/needs": "himem"
+;;         }
+;;       },
+;;       "spec": {
+;;         "nodeName": "gpu-node-1"
+;;       }
+;;     }
+;;   }
+;; }
+
+;; {
+;;   "apiVersion": "admission.k8s.io/v1",
+;;   "kind": "AdmissionReview",
+;;   "response": {
+;;     "uid": "123abc...",
+;;     "allowed": true,
+;;     "patchType": "JSONPatch",
+;;     "patch": "<base64-encoded JSON patch>"
+;;   }
+;; }
---
+++ src/gpu-device-assigner/logging.clj
@@ -0,0 +1,36 @@
+(ns gpu-device-assigner.logging)
+
+(defprotocol Logger
+  (fatal [self msg])
+  (error [self msg])
+  (warn  [self msg])
+  (info  [self msg])
+  (debug [self msg]))
+
+(def LOG-LEVELS [:fatal :error :warn :info :debug])
+(defn log-index [log-level] (.indexOf LOG-LEVELS log-level))
+
+(defn print-logger
+  [log-level]
+  (let [log-idx (log-index log-level)]
+    (reify Logger
+
+      (fatal [_ msg]
+        (when (<= log-idx (log-index :fatal))
+          (println msg)))
+
+      (error [_ msg]
+        (when (<= log-idx (log-index :error))
+          (println msg)))
+
+      (warn [_ msg]
+        (when (<= log-idx (log-index :warn))
+          (println msg)))
+
+      (info [_ msg]
+        (when (<= log-idx (log-index :info))
+          (println msg)))
+
+      (debug [_ msg]
+        (when (<= log-idx (log-index :debug))
+          (println msg))))))
