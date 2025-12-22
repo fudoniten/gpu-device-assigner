b075cbb8c198a13aee6b6fb39f14eddf6d9a0797
fudoniten <niten@fudo.org>
2025-12-22 15:37:15 -0800

    Use Timbre level handling directly

--- src/gpu_device_assigner/logging.clj
+++ src/gpu_device_assigner/logging.clj
@@ -2,46 +2,32 @@
   (:require [clojure.spec.alpha :as s]
             [taoensso.timbre :as timbre]))
 
-;; Ensure Timbre allows all levels; per-logger gating happens in this namespace.
-(timbre/merge-config! {:min-level :trace})
-
-(def LOG-LEVELS [:fatal :error :warn :info :debug])
-
-(defn log-index
-  "Return the position of `log-level` within `LOG-LEVELS`."
-  [log-level]
-  (.indexOf LOG-LEVELS log-level))
-
 (defrecord TimbreLogger [min-level])
 
 (defn print-logger
-  "Create a Timbre-backed logger that respects the provided minimum level."
+  "Create a Timbre-backed logger that leverages Timbre's built-in level handling."
   [log-level]
   (->TimbreLogger log-level))
 
-(defn- enabled?
-  [^TimbreLogger logger log-level]
-  (<= (log-index log-level) (log-index (:min-level logger))))
+(defn- with-min-level
+  [^TimbreLogger logger f]
+  (timbre/with-merged-config {:min-level (:min-level logger)}
+    (f)))
 
 (defn fatal [logger msg]
-  (when (enabled? logger :fatal)
-    (timbre/fatal msg)))
+  (with-min-level logger #(timbre/fatal msg)))
 
 (defn error [logger msg]
-  (when (enabled? logger :error)
-    (timbre/error msg)))
+  (with-min-level logger #(timbre/error msg)))
 
 (defn warn [logger msg]
-  (when (enabled? logger :warn)
-    (timbre/warn msg)))
+  (with-min-level logger #(timbre/warn msg)))
 
 (defn info [logger msg]
-  (when (enabled? logger :info)
-    (timbre/info msg)))
+  (with-min-level logger #(timbre/info msg)))
 
 (defn debug [logger msg]
-  (when (enabled? logger :debug)
-    (timbre/debug msg)))
+  (with-min-level logger #(timbre/debug msg)))
 
 (defn logger?
   "Check if an object is a Timbre-backed logger."
